a9d1da6c68918d51230b14b061077d8146365895
Ojasvi <ojasvidoye@gmail.com>
2024-09-05 12:06:10 +0530

    add asyncHandler utility function

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -1,20 +1,20 @@
 import { User } from "../models/user.model";
+import { asyncHandler } from "../utils/asyncHandler";
 import bcrypt from "bcryptjs";
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const register = async (
-  req: Request,
-  res: Response
-): Promise<Response> => {
-  try {
+export const register = asyncHandler(
+  async (req: Request, res: Response): Promise<Response> => {
     const { username, password } = req.body;
+
     const checkUsername = await User.findOne({ username });
     if (checkUsername) {
       return res.status(409).json({ msg: "username already exists" });
     }
+
     const saltRounds = 10;
     const salt = bcrypt.genSaltSync(saltRounds);
     const hashedPassword = bcrypt.hashSync(password, salt);
@@ -23,18 +23,20 @@ export const register = async (
       username,
       password: hashedPassword,
     });
-    return res.status(201).json(user);
-  } catch (error) {
-    return res.status(500).json({ msg: error });
+
+    return res.status(201).json({ msg: user });
   }
-};
-export const login = async (req: Request, res: Response): Promise<Response> => {
-  try {
+);
+
+export const login = asyncHandler(
+  async (req: Request, res: Response): Promise<Response> => {
     const { username, password } = req.body;
+
     const user = await User.findOne({ username });
     if (!user) {
       return res.status(404).json({ msg: "user not found" });
     }
+
     const isPasswordValid = bcrypt.compareSync(password, user.password);
     if (!isPasswordValid) {
       return res.status(401).json({ msg: "invalid password" });
@@ -57,18 +59,14 @@ export const login = async (req: Request, res: Response): Promise<Response> => {
         expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
       }
     );
+
     user.refreshToken = refreshToken;
     await user.save();
+
     res
-      .cookie("access_token", accessToken, {
-        httpOnly: true,
-      })
-      .cookie("refresh_token", refreshToken, {
-        httpOnly: true,
-      });
+      .cookie("access_token", accessToken, { httpOnly: true })
+      .cookie("refresh_token", refreshToken, { httpOnly: true });
 
     return res.status(200).json({ msg: "login successful" });
-  } catch (error) {
-    return res.status(500).json({ msg: error });
   }
-};
+);
